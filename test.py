2b0c50dec6afa38504a1d01c4992cc46430856ed
bostondynamics <bostondynamics@gmail.com>
2021-02-12 08:38:33 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import pyautogui
+
+print(pyautogui.position())
+
+pyautogui.click(280,50)
+
+pyautogui.hotkey("command","c")
+# pyautogui.hotkey("command","space")
+
+pyautogui.typewrite("Hello world")
+pyautogui.typewrite(["enter"])
+
+
+
